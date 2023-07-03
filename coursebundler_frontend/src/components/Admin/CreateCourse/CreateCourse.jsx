8fc4fbe31c9f899ea1e3984a35138dc569b8044b
Rashad <akyrashad@gmail.com>
2023-07-03 16:43:27 +0530

    create course page

--- coursebundler_frontend/src/components/Admin/CreateCourse/CreateCourse.jsx
+++ coursebundler_frontend/src/components/Admin/CreateCourse/CreateCourse.jsx
@@ -1,15 +1,123 @@
-import { Box, Grid } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Heading,
+  Image,
+  Input,
+  Select,
+  VStack,
+} from '@chakra-ui/react';
 import cursor from '../../../assets/images/cursor.png';
 import Sidebar from '../Sidebar';
+import { useState } from 'react';
+import { fileUploadCss } from '../../Auth/Register';
 
 const CreateCourse = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [createdBy, setCreatedBy] = useState('');
+  const [category, setCategory] = useState('');
+  const [image, setImage] = useState('');
+  const [imagePrev, setImagePrev] = useState('');
+
+  const categories = [
+    'Web-development',
+    'Artificial Intelligence',
+    'Data Structure & Algorithm',
+    'App Development',
+    'Data Science',
+    'Game Development',
+  ];
+
+  const changeImageHandler = e => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setImagePrev(reader.result);
+      setImage(file);
+    };
+  };
   return (
     <Grid
       minH={'100vh'}
       css={{ cursor: `url(${cursor}),default` }}
       templateColumns={['1fr', '5fr 1fr']}
     >
-      <Box></Box>
+      <Container py={16}>
+        <form>
+          <Heading
+            textTransform={'uppercase'}
+            children="Create Course"
+            my={16}
+            textAlign={['center', 'left']}
+          />
+          <VStack m={'auto'} spacing={8}>
+            <Input
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+              placeholder="Title"
+              type="text"
+              focusBorderColor="purple.400"
+            />
+
+            <Input
+              value={description}
+              onChange={e => setDescription(e.target.value)}
+              placeholder="Description"
+              type="text"
+              focusBorderColor="purple.400"
+            />
+
+            <Input
+              value={createdBy}
+              onChange={e => setCreatedBy(e.target.value)}
+              placeholder="Creator Name"
+              type="text"
+              focusBorderColor="purple.400"
+            />
+
+            <Select
+              focusBorderColor="purple.300"
+              value={category}
+              onChange={e => setCategory(e.target.value)}
+            >
+              <option disabled value={''}>
+                Category
+              </option>
+
+              {categories.map((item, index) => (
+                <option key={index} value={item}>
+                  {item}
+                </option>
+              ))}
+            </Select>
+
+            <Input
+              accept="image/*"
+              required
+              id="chooseAvatar"
+              type="file"
+              focusBorderColor="purple.500"
+              css={{
+                '&::file-selector-button': {
+                  ...fileUploadCss,
+                  color: 'purple',
+                },
+              }}
+              onChange={changeImageHandler}
+            />
+            {imagePrev && (
+              <Image src={imagePrev} boxSize={64} objectFit={'contain'} />
+            )}
+            <Button w={'full'} colorScheme="purple" type="submit">
+              Create
+            </Button>
+          </VStack>
+        </form>
+      </Container>
       <Sidebar />
     </Grid>
   );
