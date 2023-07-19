f3ba41b3f4bf62abde93db3e14a082f087846be0
Rashad <akyrashad@gmail.com>
2023-07-19 09:30:37 +0530

    razor pay integration

--- coursebundlerserver/app.js
+++ coursebundlerserver/app.js
@@ -19,9 +19,11 @@ app.use(cookieParser());
 //Importing & Using Routes
 import course from "./routes/courseRoutes.js";
 import user from "./routes/userRoutes.js";
+import payment from "./routes/paymentRoutes.js";
 import { ErrorMiddleware } from "./middlewares/Error.js";
 app.use("/api/v1", course);
 app.use("/api/v1", user);
+app.use("/api/v1", payment);
 
 export default app;
 
