aa48bf6279ac0771989c0c4bed2cbc45f82616ec
rashad <akyrashad@gmail.com>
2023-07-22 07:53:38 +0530

    new fix

--- coursebundlerserver/app.js
+++ coursebundlerserver/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 config({
 	path: "./config/config.env",
 });
@@ -15,6 +15,13 @@ app.use(
 	})
 );
 app.use(cookieParser());
+app.use(
+	cors({
+		origin: process.env.FRONTEND_URL,
+		credential: true,
+		methods: ["GET", "POST", "PUT", "DELETE"],
+	})
+);
 
 //Importing & Using Routes
 import course from "./routes/courseRoutes.js";
@@ -27,6 +34,11 @@ app.use("/api/v1", course);
 app.use("/api/v1", user);
 app.use("/api/v1", payment);
 app.use("/api/v1", otherRoutes);
+app.get("/", (req, res) =>
+	res.send(
+		`<h1>Server is Working. click <a href=${process.env.FRONTEND_URL}>Here</a></h1>`
+	)
+);
 
 export default app;
 
